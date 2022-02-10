4980c6b8c825e5c5eefcb95287df9c620b81968e
User <User@WIN-19OSRQ3CMLT>
2022-02-10 09:43:28 +0300

    Добавьте файлы проекта.

---
+++ Передача параметров приложению в консоли/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Передача_параметров_приложению_в_консоли
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
