7e5b138995ce3f93a2df2d9270a4f82e9d0ef5e2
MishanyaGG <mischatitovmail.ru@gmail.com>
2022-02-10 15:51:09 +0300

    Вторая задача

---
+++ Задача 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            int a = Convert.ToInt32(args[0]);
+            int b = Convert.ToInt32(args[1]);
+            Console.WriteLine(a+b);
+        }
+    }
+}
