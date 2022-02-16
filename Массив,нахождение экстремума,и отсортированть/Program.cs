b440b1327d4f2f7569de1f8248d8024a35beca55
MishanyaGG <mischatitovmail.ru@gmail.com>
2022-02-16 18:25:27 +0300

    Определить в каком числе больше цифр

---
+++ Массив,нахождение экстремума,и отсортированть/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Массив_нахождение_экстремума_и_отсортированть
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов в массиве");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[a];
+            Random rand = new Random();
+            int min = int.MaxValue; //int.MaxValue = 2 147 483 647
+            int max = int.MinValue; //int.MinValue = -2 147 483 647
+            int index_min = 0;
+            int index_max = 0;
+            for (int x = 0; x < array.Length; x++)
+            {
+                array[x] = rand.Next(20); //Пока х меньше количество элементов массива, то х увелчивается на 1
+                Console.Write(array[x] + " ");
+            }
+            Console.WriteLine("\nИндексы");
+            for (int x = 0; x < array.Length; x++)
+            {
+                Console.WriteLine($"{x}-ый элемент массива равен {array[x]}");
+                if (array[x] > max)
+                {
+                    max = array[x];
+                    index_max = x;
+                }
+                if (array [x] <min)
+                {
+                    min = array[x];
+                    index_min = x;
+                }
+            }
+
+            Console.WriteLine("\n***********************************************************");
+            Console.WriteLine($"Минимальное значение в массиве = {min}, максимальное значение в массиве = {max}");
+            Console.WriteLine($"Разность, между максимальным и минимальным значением в массиве {Math.Abs(index_max - index_min)}");
+            
+            int vrem; //Временная переменная
+            //BubbleSort
+            for (int x = 0; x<array.Length; x++)
+            {
+                for (int j = x+1; j<array.Length; j++)
+                {
+                    if (array[x]>array[j])
+                    {
+                        vrem = array[x];
+                        array[x] = array[j];
+                        array[j] = vrem;
+                    }
+                }
+            }
+            Console.WriteLine("Соритровка массива");
+            Console.WriteLine();
+            for(int x = 0; x<array.Length;x++)
+            {
+                Console.Write(array[x] + " ");
+            }
+            Console.WriteLine("\n***********************************************************");
+        }
+    }
+}
--- Определить в каком числе больше цифр/Program.cs
+++ Определить в каком числе больше цифр/Program.cs
@@ -4,32 +4,35 @@ namespace Определить_в_каком_числе_больше_цифр
 {
     class resh
     {
-        public void Reshenie(int a, int b)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public int count_a(int a)
         {
-            int new_a = a;
-            int new_b = b;
-            int n = 0;
-            int k = 0;
-
-            while (a > 0)
+            int n = 0; //Переменная, которая будет определять количество цифр в первом числе
+            while (Math.Abs(a) > 0)
             {
                 a = a / 10;
-                n = n + 1;
+                n++;
             }
-            while (b > 0)
+            return n;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int count_b(int b)
+        {
+            int k = 0; //Переменная, которая будет определять количество цифр во втором числе
+            while (Math.Abs(b) > 0)
             {
                 b = b / 10;
-                k = k + 1;
+                k++;
             }
-            if (n > k)
-                Console.WriteLine($"В числе {new_a} больше цифр, чем в {new_b}. Количество цифр {new_a} = {n}, {new_b} = {k}");
-            else
-            {
-                if (n<k)
-                    Console.WriteLine($"В числе {new_b} больше цифр, чем в {new_a}. Количество цифр {new_a} = {n}, {new_b} = {k}");
-                else
-                    Console.WriteLine($"В числах {new_a} и {new_b} одинаковое количество цифр");
-            }
+            return k;
         }
         class Program
         {
@@ -38,10 +41,20 @@ namespace Определить_в_каком_числе_больше_цифр
                 Console.WriteLine("Определение в каком числе больше цифр");
                 resh snach = new resh();
                 Console.WriteLine("Введите первое число");
-                int a = Convert.ToInt32(Console.ReadLine());
+                int a = Convert.ToInt32(Console.ReadLine()); //Первое число
                 Console.WriteLine("Введте второе число");
-                int b = Convert.ToInt32(Console.ReadLine());
-                snach.Reshenie(a, b);
+                int b = Convert.ToInt32(Console.ReadLine()); //Второе число
+                
+                
+                if (snach.count_a(a) > snach.count_b(b))
+                    Console.WriteLine($"В числе {a} больше цифр, чем в {b}. Количество цифр {a} = {snach.count_a(a)}, {b} = {snach.count_b(b)}");
+                else
+                {
+                    if (snach.count_a(a) < snach.count_b(b))
+                        Console.WriteLine($"В числе {b} больше цифр, чем в {a}. Количество цифр {a} = {snach.count_a(a)}, {b} = {snach.count_b(b)}");
+                    else
+                        Console.WriteLine($"В числах {a} и {b} одинаковое количество цифр");
+                }
             }
         }
     }
